af0a2574b6451010cbc1e5e6889634f7b2544d39
Axm <56112475+ax-sh@users.noreply.github.com>
2021-10-19 11:37:21 +0545

    hide context menu

--- main.py
+++ main.py
@@ -39,18 +39,18 @@ class App(QApplication):
 def main():
     app = App([])
     tray = TrayIcon()
-    # Create the menu
-    menu = QMenu()
-    action = QAction("A menu item")
-    menu.addAction(action)
+    # # Create the menu
+    # menu = QMenu()
+    # action = QAction("A menu item")
+    # menu.addAction(action)
 
-    # Add a Quit option to the menu.
-    quit = QAction("Quit")
-    quit.triggered.connect(app.quit)
-    menu.addAction(quit)
+    # # Add a Quit option to the menu.
+    # quit = QAction("Quit")
+    # quit.triggered.connect(app.quit)
+    # menu.addAction(quit)
 
     # Add the menu to the tray
-    tray.setContextMenu(menu)
+    # tray.setContextMenu(menu)
     app.exec_()
    
 
